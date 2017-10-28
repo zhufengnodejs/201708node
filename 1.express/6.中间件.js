ceef2222e7938d974cd0d190f94ca1d16aa7296c
zhufengnodejs <zhufengnodejs@126.com>
2017-10-28 15:34:16 +0800

    express

--- 1.express/6.中间件.js
+++ 1.express/6.中间件.js
@@ -3,6 +3,7 @@
  */
 let express = require('express');
 let app = express();
+let fs = require('fs');
 /**
  * 中间件其实是一个函数
  * use表示使用一个中间件函数
@@ -14,13 +15,25 @@ app.use(function(req,res,next){
   res.header('Content-Type','text/html;charset=utf8');
   //因为我们的代码中可能会有异步逻辑，不能认为代码执行结束整个任务就结束了.
   //所以需要调用next方法来表示任务执行结束
-  setTimeout(function(){
-    next();
-  },3000);
+  // setTimeout(function(){
+  //   next();
+  // },3000);
+  //读取一个文件的内容，并把读到的结果赋给req.msg
+  fs.readFile('1.txt','utf8',function(err,data){
+    //如果在中间件中出错的话应该交给统一的处理函数来处理
+    if(err){
+      //同样调用next继续执行，但是会传入错误对象
+      //如果next的参数不为null就表示有错误了，则会跳过正常的业务逻辑，交由错误处理中间件来处理
+      next(err);
+    }else{
+      req.msg = data;
+      next();
+    }
+
+  });
 });
 app.use(function(req,res,next){
   console.log('中间件2');
-  res.header('Content-Type','text/html;charset=utf8');
   next();
 });
 app.get('/',function(req,res){
@@ -28,5 +41,9 @@ app.get('/',function(req,res){
 });
 app.get('/user',function(req,res){
   res.end('用户管理');
+});
+//错误处理中间件多了一个参数，err
+app.use(function(err,req,res,next){
+  console.error(err);
 });
 app.listen(8080);
