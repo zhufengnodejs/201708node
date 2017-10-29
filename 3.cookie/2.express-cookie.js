31ff4910235086d2da1eaae0faccd2483754d704
zhufengnodejs <zhufengnodejs@126.com>
2017-10-29 12:25:04 +0800

    express

--- 3.cookie/2.express-cookie.js
+++ 3.cookie/2.express-cookie.js
@@ -6,15 +6,22 @@ let app = express();
 app.use(cookieParser());
 app.get('/write',function(req,res){
   res.cookie('name','zfpx');
-  res.send('write ok')
+  res.send('write ok');
 });
 
 app.get('/read',function(req,res){
    // name=zfpx querystring.parse(str);
    res.json(req.cookies);
 });
+//统计每个客户端访问服务器的次数
 app.get('/visit',function(req,res){
-
+   //1.先试图获取客户端的cookie里的访问次数
+   let visit = req.cookies.visit||0;
+   //2.把访问次数+1
+   visit++;
+   //3.把最新的访问次数写回客户端
+   res.cookie('visit',visit);
+   res.send(`欢迎你的第${visit}次访问`);
 });
 //
 app.listen(8080);
