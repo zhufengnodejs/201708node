960e1710c7e33d542dc19afe414a239e854b0e6f
zhufengnodejs <zhufengnodejs@126.com>
2017-10-29 12:53:47 +0800

    express

--- 3.cookie/2.express-cookie.js
+++ 3.cookie/2.express-cookie.js
@@ -5,7 +5,15 @@ let app = express();
 //使用此中间件之后，req请求对象多了一个cookies属性
 app.use(cookieParser());
 app.get('/write',function(req,res){
-  res.cookie('name','zfpx');
+  //1.不指定任何参数
+  //res.cookie('name','zfpx');
+  //2.指定域名，指定这个cookie归谁所有,当向哪个域名发请求的时候才会带上此cookie,默认值是当前域名
+  // a.zfpx.cn b.zfpx.cn 都指向本地127.0.0.1
+  // 配置host文件
+  //Set-Cookie:name=zfpx; Domain=a.zfpx.cn; Path=/
+  res.cookie('name','zfpx',{
+    domain:'a.zfpx.cn'
+  });
   res.send('write ok');
 });
 
@@ -22,6 +30,11 @@ app.get('/visit',function(req,res){
    //3.把最新的访问次数写回客户端
    res.cookie('visit',visit);
    res.send(`欢迎你的第${visit}次访问`);
+});
+app.get('/clear',function(req,res){
+  // Set-Cookie visit=
+  res.clearCookie('visit');
+  res.send('重置访问次数');
 });
 //
 app.listen(8080);
