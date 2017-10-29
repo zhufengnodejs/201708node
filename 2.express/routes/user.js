a4b97d3244b754dadde0df7c79487a6b3932e79a
zhufengnodejs <zhufengnodejs@126.com>
2017-10-29 10:19:23 +0800

    express

--- 2.express/routes/user.js
+++ 2.express/routes/user.js
@@ -4,3 +4,10 @@
  * GET /user/signin
  * POST /user/signin
  **/
+let express = require('express');
+let router = express.Router();
+router.get('/signup',function(req,res){
+  //此路径是相对于模板根目录的子路径，后缀是可以省略
+  res.render('signup',{title:'用户注册'});
+});
+module.exports = router;
--- 2.express/server.js
+++ 2.express/server.js
@@ -1,5 +1,17 @@
-let express = require('express');
-let app = express();
-let index = require('./routes/index');
+const express = require('express');
+const app = express();
+const index = require('./routes/index');
+const user = require('./routes/user');
+let path  = require('path');
+//设置模板引擎 决定模板后缀
+app.set('view engine','html');
+//设置模板的存放根目录
+app.set('views',path.resolve('views'));
+//设置模板的渲染方法
+app.engine('html',require('ejs').__express);
+//res.render
+//当请求路径以/开头的时候，走index中间件
 app.use('/',index);
+//当请求路径以/user开头的时候，走user中间件
+app.use('/user',user);
 app.listen(8080);
